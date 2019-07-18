b6a177260c56d47f8e59f3487ba1489542b245a6
shawn luah <17010564@myrp.edu.sg>
2019-07-18 16:12:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/demoaddon/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.demoaddon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+        iv.setImageResource(R.mipmap.ic_launcher);
+
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
+    }
+}
